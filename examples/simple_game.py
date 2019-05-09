bdc4c2b9a8cf6fe6842d8e6eb11ca307b74af48b
colorfuldisaster <thecolorfuldisaster@gmail.com>
2019-05-09 20:38:48 +0300

    added ability to print grids

--- examples/simple_game.py
+++ examples/simple_game.py
@@ -1,6 +1,7 @@
-import battleship.ship
-import battleship.player
 import battleship.game
+import battleship.grid
+import battleship.player
+import battleship.ship
 
 GAME_HEIGHT = 8
 GAME_WIDTH = 8
@@ -14,6 +15,24 @@ def print_results(hits, ships_sunk, players_lost):
         print('{}\'s {} was sunk at {}'.format(id(player), ship.__class__.__name__, list(ship.get_segments())))
     for player in players_lost:
         print('{} has lost'.format(id(player)))
+
+
+def print_grid(grid):
+    marks = grid.get_marks()
+    for i in range(GAME_HEIGHT):
+        for j in range(GAME_WIDTH):
+            for mark in marks:
+                if (mark.x, mark.y) == (i, j):
+                    if isinstance(mark, battleship.grid.RedPeg):
+                        print('X', end='')
+                    elif isinstance(mark, battleship.grid.WhitePeg):
+                        print('O', end='')
+                    else:
+                        print('-', end='')
+                    break
+            else:
+                print('-', end='')
+        print()
 
 
 def main():
@@ -54,6 +73,29 @@ def main():
     print('Round 3:')
     print()
     print_results(*g.process_shots([s2, s3]))
+
+    print()
+    print('Final public grid:')
+    print()
+
+    print_grid(g.get_public_grid())
+
+    print()
+    print('Final private grid:')
+    print()
+
+    print_grid(g.get_private_grid())
+
+    print()
+    print('Final player grids:')
+    print()
+
+    print_grid(p1.get_grid())
+    print()
+    print_grid(p2.get_grid())
+    print()
+    print_grid(p3.get_grid())
+    print()
 
 if __name__ == '__main__':
     main()
