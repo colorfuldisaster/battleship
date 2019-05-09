baa101864adc8ab7017e7f09899d72c6ae84b2a7
colorfuldisaster <thecolorfuldisaster@gmail.com>
2019-05-09 22:07:17 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='battleship',
+        version='1.0',
+        description='Implementation for the battleship board game, extended for any number of players',
+        license='MIT',
+        author='colorfuldisaster',
+        author_email='thecolorfuldisaster@gmail.com',
+        packages=['battleship'],
+)
