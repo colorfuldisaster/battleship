77f391c599abe12577d223c5bc59c0909dd14096
colorfuldisaster <thecolorfuldisaster@gmail.com>
2019-05-09 19:58:42 +0300

    revamped the way sunk ships and lost players are computed in process_shots

--- battleship/game.py
+++ battleship/game.py
@@ -14,15 +14,24 @@ class Game(object):
 
     def process_shots(self, shots):
         successful_shots = []
-        ships_sunk_before = sum([player.get_sunk_ships() for player in self._players], [])
-        players_lost_before = [player for player in self._players if not player.has_ships_remaining()]
+        ships_sunk = set()
+        players_lost = set()
+
+        alive_players = [player for player in self._players if player.has_ships_remaining()]
 
         for shot in shots:
-            for player in self._players:
+            for player in alive_players:
                 if player.has_ship_at(shot.x, shot.y):
+                    ships_sunk_before = set(player.get_sunk_ships())
                     player.mark_ship_hit(shot.x, shot.y)
-                    # Add to list of successful shots, to be announced later
-                    successful_shots.append((shot, player))
+                    ships_sunk_delta = set(player.get_sunk_ships()) - ships_sunk_before
+
+                    # Update results: hits, sinks, losers
+                    successful_shots.append((shot, player)) # (hit, player which got hit)
+                    for ship in ships_sunk_delta:
+                        ships_sunk.add((ship, player))      # (ship, ship's owner)
+                    if not player.has_ships_remaining():
+                        players_lost.add(player)
 
             # Was any player's ship hit?
             if any([player.has_ship_at(shot.x, shot.y) for player in self._players]):
@@ -39,11 +48,5 @@ class Game(object):
                 else:
                     # Mark the "miss" publicly
                     self._public_grid.mark(grid.WhitePeg(shot.x, shot.y))
-
-        ships_sunk_after = sum([player.get_sunk_ships() for player in self._players], [])
-        ships_sunk = set(ships_sunk_after) - set(ships_sunk_before)
-
-        players_lost_after = [player for player in self._players if not player.has_ships_remaining()]
-        players_lost = set(players_lost_after) - set(players_lost_before)
 
         return successful_shots, ships_sunk, players_lost
